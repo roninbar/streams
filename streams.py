b4aecaba2096647764835c5ab3ea7d2a380f3f8c
ron <rinbar70@gmail.com>
2022-01-22 21:39:40 +0200

    Add `stream_map()`.

--- streams.py
+++ streams.py
@@ -1,3 +1,4 @@
+import operator
 from typing import Callable, TypeAlias, TypeVar, Any
 
 T = TypeVar('T')
@@ -5,7 +6,6 @@ T = TypeVar('T')
 Promise: TypeAlias = Callable[[], T]
 
 Stream: TypeAlias = tuple[T, Promise[Any]]  # Actually, tuple[T, Promise[Stream[T]]]
-
 
 the_empty_stream = ()
 
@@ -30,9 +30,9 @@ def stream_rest(s: Stream[T]) -> Stream[T]:
     return force(s[1])
 
 
-def stream_enumerate_interval(low: int, high: int) -> Stream[int]:
-    if low < high:
-        return cons_stream(low, lambda: stream_enumerate_interval(low + 1, high))
+def stream_enumerate_interval(start: int, finish: int, step: int = 1) -> Stream[int]:
+    if start < finish if step > 0 else finish < start:
+        return cons_stream(start, lambda: stream_enumerate_interval(start + step, finish, step))
     else:
         return the_empty_stream
 
@@ -53,6 +53,15 @@ def stream_filter(pred: Callable[[T], bool], s: Stream[T]) -> Stream[T]:
         return stream_filter(pred, stream_rest(s))
 
 
-if __name__ == '__main__':
-    stream_for_each(print, stream_enumerate_interval(10, 20))
+def stream_map(proc: Callable[[], T], *ss: list[Stream]) -> Stream[T]:
+    if stream_null(ss[0]):
+        return the_empty_stream
+    else:
+        return cons_stream(proc(*[stream_first(s) for s in ss]),
+                           lambda: stream_map(proc, *[stream_rest(s) for s in ss]))
 
+
+if __name__ == '__main__':
+    stream_for_each(print, stream_map(operator.add,
+                                      stream_enumerate_interval(10, 20),
+                                      stream_enumerate_interval(30, 40)))
