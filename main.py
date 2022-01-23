19783b9ad4718627cf8c4a1284ef2ddb9b7b5c47
ron <rinbar70@gmail.com>
2022-01-23 21:37:14 +0200

    Compute `primes` as an infinite stream.

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-import streams
+import operator
+
+from streams import Stream, foreach, filter, cons, first, rest, count, map
 
 
 def is_prime(n: int) -> bool:
     if n < 2:
         raise ValueError(f'Argument `n` must be at least 2. {n} received.')
     else:
-        p = 2
-        while p * p <= n:
-            if n % p == 0:
-                break
-            p += 1
+        ps = primes
+        while (p := first(ps)) ** 2 <= n and n % p != 0:
+            ps = rest(ps)
         else:
-            return True
-        return False
+            return p * p > n
 
+
+def sieve(s: Stream[int]) -> Stream:
+    return cons(first(s),
+                lambda: sieve(filter(lambda x: x % first(s) != 0,
+                                     rest(s))))
+
+
+def add(s1: Stream[int], s2: Stream[int]):
+    return map(operator.add, s1, s2)
+
+
+ones = cons(1, lambda: ones)
+
+integers = cons(1, lambda: add(ones, integers))
+
+fibs = cons(0, lambda: cons(1, lambda: add(rest(fibs), fibs)))
+
+primes = cons(2, lambda: filter(is_prime, count(3)))
 
 if __name__ == '__main__':
-    streams.foreach(lambda p: print(f'{p:,d}'),
-                    streams.filter(is_prime,
-                                   streams.range(10_000, 100_000)))
+    foreach(lambda p: print(f'{p:,d}'), primes)
